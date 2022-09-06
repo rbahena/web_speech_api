f4b27fb001f54f49942b6a3cf56e811fdc2c8bbf
rbahena <rbahena@cjf.gob.mx>
2022-09-06 17:41:23 -0500

    [feat]: add speak function

--- js/index.js
+++ js/index.js
@@ -1,11 +1,90 @@
-if (!'speechSynthesis' in window) {
-    showNotify({
-      msn: 'Your browser not support tha Web Speech API',
-      show: true,
-      type: 'success'
-    });
-    return false;
+// Reference: https://www.ebooz.com/como-anadir-un-sintetizador-de-texto-a-voz-de-audio-a-tu-pagina-web/
+
+onload = function () {
+  if ("speechSynthesis" in window) {
+    /* speech synthesis supported */
+    alert("Soy compatible");
+  } else {
+    alert("No soy compatible");
+
+    /* speech synthesis not supported */
   }
 
-  let voice = new SpeechSynthesisUtterance();
-  let jarvis = window.speechSynthesis;
+  if ("speechSynthesis" in window) {
+    var synth = speechSynthesis;
+    var flag = false;
+
+    /* references to the buttons */
+    var playEle = document.querySelector("#play");
+    var pauseEle = document.querySelector("#pause");
+    var stopEle = document.querySelector("#stop");
+
+    /* click event handlers for the buttons */
+    playEle.addEventListener("click", onClickPlay);
+    pauseEle.addEventListener("click", onClickPause);
+    stopEle.addEventListener("click", onClickStop);
+
+    function onClickPlay() {
+      if (!flag) {
+        flag = true;
+        utterance = new SpeechSynthesisUtterance(
+          document.querySelector("article").textContent
+        );
+        utterance.voice = synth.getVoices()[1];
+        utterance.onend = function () {
+          flag = false;
+        };
+        synth.speak(utterance);
+      }
+      if (synth.paused) {
+        /* unpause/resume narration */
+        synth.resume();
+      }
+    }
+    function onClickPause() {
+      if (synth.speaking && !synth.paused) {
+        /* pause narration */
+        synth.pause();
+      }
+    }
+    function onClickStop() {
+      if (synth.speaking) {
+        /* stop narration */
+        /* for safari */
+        flag = false;
+        synth.cancel();
+      }
+    }
+  }
+};
+
+//#region get_voices
+function populateVoiceList() {
+  if (typeof speechSynthesis === "undefined") {
+    return;
+  }
+
+  const voices = speechSynthesis.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    document.getElementById("voiceSelect").appendChild(option);
+  }
+}
+
+populateVoiceList();
+if (
+  typeof speechSynthesis !== "undefined" &&
+  speechSynthesis.onvoiceschanged !== undefined
+) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+//#endregion
